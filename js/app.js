6a50cedd96f198fb175d1d6f34417ffe0a0bb001
shshishi19 <shishir.cnits@gmail.com>
2025-11-24 20:58:14 +0600

    project done

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    // banner-Slider
+
+
+    // Review-Slider
+
+
+    $('#testimonial').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: true,
+        items: 1,
+        startSpeed: 1000,
+        
+    })
+});
